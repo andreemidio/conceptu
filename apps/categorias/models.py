6a5807e19a903811d593fa094c94997694c7392a
Andre Emidio <andresjc2008@gmail.com>
2021-01-25 21:30:27 -0300

    Correção da view do banco e serializers

--- apps/categorias/models.py
+++ apps/categorias/models.py
@@ -9,5 +9,18 @@ from apps.usuarios.models import Usuarios
 class Categorias(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome_categoria = models.CharField(max_length=255)
+    data_criacao = models.DateTimeField(auto_now_add=True)
     criado_por = models.ForeignKey(
         Usuarios, on_delete=models.DO_NOTHING, blank=True, null=True, related_name='categorias_criado_por_usuarios')
+
+    def __str__(self):
+        return self.nome_categoria
+
+    def __repr__(self):
+        return self.nome_categoria
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['-data_criacao']
+
--- apps/produtos/models.py
+++ apps/produtos/models.py
@@ -12,6 +12,18 @@ class Produtos(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    data_criacao = models.DateTimeField(auto_now_add=True)
     categorias = models.ManyToManyField(Categorias)
     criado_por = models.ForeignKey(
         Usuarios, on_delete=models.DO_NOTHING, blank=True, null=True, related_name='produtos_criado_por_usuarios')
+
+    def __str__(self):
+        return self.nome
+
+    def __repr__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+        ordering = ['-data_criacao']
