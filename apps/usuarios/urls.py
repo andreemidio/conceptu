22ff78daff6e511c74f96aa236d62a0131af1eaf
Andre Emidio <andresjc2008@gmail.com>
2021-01-26 08:20:15 -0300

    Correção da queryset da criação da categoria

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from apps.usuarios.viewsets import PostUsuariosViewSets
+from apps.usuarios.viewsets import PostUsuariosViewSets, ListUsuariosViewSets, UpdateUsuariosViewSets, \
+    RetriveUsuariosViewSets, DeleteUsuariosViewSets
 
 app_name = 'usuarios'
 
 router = routers.DefaultRouter()
 router.register(r'cadastrar-usuarios', PostUsuariosViewSets, basename='cadastrar-usuarios')
-router.register(r'listar-usuarios', PostUsuariosViewSets, basename='cadastrar-usuarios')
-router.register(r'detalhes-usuario', PostUsuariosViewSets, basename='cadastrar-usuarios')
-router.register(r'deletar-usuario', PostUsuariosViewSets, basename='cadastrar-usuarios')
+router.register(r'listar-usuarios', ListUsuariosViewSets, basename='cadastrar-usuarios')
+router.register(r'detalhes-usuario', RetriveUsuariosViewSets, basename='cadastrar-usuarios')
+router.register(r'atualizar-usuario', UpdateUsuariosViewSets, basename='cadastrar-usuarios')
+router.register(r'deletar-usuario', DeleteUsuariosViewSets, basename='cadastrar-usuarios')
 
 urlpatterns = [
     path(r'', include(router.urls)),
